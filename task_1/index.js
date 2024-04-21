92502bb24ea7124ba9ca9638ab9e85db922c7f82
Daniel Kudinov <danielkudinov17@gmail.com>
2024-04-22 01:11:05 +0300

    add task1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,20 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from './data.js'
 
 console.log("Let's rock")
 console.log(encoded, translations)
 
-
-
-// console.log(decoded)
+export const decodeFields = (encoded, translations) => {
+    return encoded.map((item)=>{
+        const decodedItem = {...item}
+        for(const key in decodedItem){
+            if(key.endsWith('Id') && !['groupId', 'service', 'formatSize', 'ca'].includes(key)){
+                const id = decodedItem[key]
+                decodedItem[key] = translations[id] || id
+            }
+            return decodedItem
+        }
+    })
+}
+const decoded = decodeFields(encoded, translations);
+console.log(decoded)
+console.log('end')
